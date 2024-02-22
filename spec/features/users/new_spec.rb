0549c7f39edcea422c9556238efe23ed39023618
Scott DeVoss <scottdevoss@yahoo.com>
2024-02-22 16:48:28 -0700

    create user test framework

---
+++ spec/features/users/new_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+RSpec.describe "Create a User" do
+  it "should desribe the create a user page" do
+    visit "/register"
+
+    expect(page).to have_content("Create User Profile")
+
+    fill_in :name, with: "Tiger Woods"
+    fill_in :email, with: "tiger@gmail.com"
+    fill_in :password, with: "golf123"
+    fill_in :password_confirmation, with: "golf123"
+
+    click_button "Create Profile"
+
+    expect(current_path).to eq(user_path(User.last.id))
+  end
+end
