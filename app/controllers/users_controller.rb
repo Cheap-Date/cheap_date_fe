f0f6941f3d7e691475a36c9dde717d67f04be809
Scott DeVoss <scottdevoss@yahoo.com>
2024-02-22 18:46:26 -0700

    added sad path test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,9 +10,10 @@ class UsersController < ApplicationController
   def create
     conn = Faraday.new(url: "http://localhost:3000") do |faraday|
       faraday.headers["Content-Type"] = "application/json"
-      faraday.headers = { name: params[:full_name], email: params[:email], password: params[:password]}
+      faraday.headers = { name: params[:name], email: params[:email], password: params[:password]}
     end
     response = conn.post("/api/v1/users")
     json = JSON.parse(response.body, symbolize_names: true)
+    # require 'pry'; binding.pry
   end
 end
--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -1,19 +1,37 @@
 require "rails_helper"
 
 RSpec.describe "Create a User" do
-  it "should desribe the create a user page" do
-    visit "/register"
+  describe "Happy Path" do
+    it "should desribe the create a user page" do
+      visit "/register"
 
-    expect(page).to have_content("Create User Profile")
+      expect(page).to have_content("Create User Profile")
 
-    fill_in :full_name, with: "Tiger Woods"
-    fill_in :email, with: "tiger@gmail.com"
-    fill_in :password, with: "golf123"
-    fill_in :password_confirmation, with: "golf123"
+      fill_in :name, with: "Tiger Woods"
+      fill_in :email, with: "tiger@gmail.com"
+      fill_in :password, with: "golf123"
+      fill_in :password_confirmation, with: "golf123"
 
-    click_button "Create Profile"
+      click_button "Create Profile"
 
-    expect(current_path).to eq(user_path(User.last.id))
-    expect(page).to have_content("Account Created")
+      expect(current_path).to eq(user_path(User.last.id))
+      expect(page).to have_content("Profile Created")
+    end
+  end
+
+  describe "Sad Path" do
+    it "gives an error if passwords do not match" do
+      visit "/register"
+
+      fill_in :name, with: "Tiger Woods"
+      fill_in :email, with: "tiger@gmail.com"
+      fill_in :password, with: "golf123"
+      fill_in :password_confirmation, with: "wrongpassword"
+
+      click_button "Create Profile"
+
+      expect(current_path).to eq("/register")
+      expect(page).to have_content("Password does not match") 
+    end
   end
 end
