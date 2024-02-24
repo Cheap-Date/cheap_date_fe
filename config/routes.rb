d23b6a999a49288825f7288046b5623dcf3a1991
Nathan Lambertson <lambertones@gmail.com>
2024-02-24 14:24:11 -0600

    Organize routes a tad bit

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,23 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
   root "welcome#index"
+
   get "/login", to: "users#login_form"
   post "/login", to: "users#login_user"
+  delete "/users/logout", to: "users#logout_user"
+
   get "/register", to: "users#new"
+  
   get "/users/:id", to: "users#show"
   post "/users", to: "users#create"
   get "/users/:id/edit", to: "users#edit"
+  patch "/users/:id", to: "users#update"
+
   get "/users/:id/dates", to: "dates#index"
   get "/users/:id/dates/new", to: "dates#new"
+
   get "/users/:id/events/new", to: "events#new"
+
   get "/users/:id/foods/new", to: "foods#new"
-  delete "/users/logout", to: "users#logout_user"
-  patch "/users/:id", to: "users#update"
 end
