c653c06fe27fba8f8fcc699128765524541b4f66
Nathan Lambertson <lambertones@gmail.com>
2024-02-24 07:28:08 -0600

    Add find-by-email method to Facade

--- app/facades/user_facade.rb
+++ app/facades/user_facade.rb
@@ -4,4 +4,11 @@ class UserFacade
     response = service.user_find(id)
     user = User.new(response)
   end
+
+  def find_by_email(email)
+    service = UserService.new
+    response = service.all_users
+    data = response.find { |u| u[:attributes][:email] == email }
+    user = User.new(data)
+  end
 end
