3271bdb1bfeb02d5d38596546f8b826735e7711e
Nathan Lambertson <lambertones@gmail.com>
2024-02-24 07:26:23 -0600

    Add id expectation to poros test

--- spec/poros/user_poros_spec.rb
+++ spec/poros/user_poros_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe User do
 
     user = User.new(attrs)
 
+    expect(user.id).to eq("1")
     expect(user.name).to eq("Tiger Woods")
     expect(user.email).to eq("tiger@gmail.com")
   end
