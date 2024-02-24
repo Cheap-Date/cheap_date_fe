38fc723646b5d96ea57d582df933b5a02a2bbc96
Nathan Lambertson <lambertones@gmail.com>
2024-02-24 07:27:02 -0600

    Alter test naming

--- spec/facades/user_facade_spec.rb
+++ spec/facades/user_facade_spec.rb
@@ -9,9 +9,17 @@ RSpec.describe UserFacade do
     end
   end
 
-  it "returns single user" do
+  it "returns single user by id" do
     user = UserFacade.new.user(1)
 
     expect(user).to be_a(User)
+  end
+
+  it "returns a single user by email" do
+    user = UserFacade.new.find_by_email("g@goo.com")
+
+    expect(user).to be_a(User)
+    expect(user.name).to eq("bob")
+    expect(user.email).to eq("g@goo.com")
   end
 end
