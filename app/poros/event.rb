2c593e8aa093b7d9b16e84137177436b6b83e03e
Matt Darlington <matthewsdarlington@gmail.com>
2024-03-02 18:14:28 -0700

    Parsing update after upstream refactor updates to keep automated Event poros creation runnnig smoothly

--- app/poros/event.rb
+++ app/poros/event.rb
@@ -1,7 +1,18 @@
 class Event
-  attr_reader :name
+  attr_reader :id, :name, :location, :description, :start_date,
+              :category, :cost, :cost_max, :is_free, :event_url
 
   def initialize(attrs)
-    @name = attrs[:name]
+    # attrs has 3 keys: id:string, type:string atttributes:{key:value} pairs
+    @id = attrs[:id]
+    @type = attrs[:type]
+    @name = attrs[:attributes][:name]
+    @location = attrs[:attributes][:location]
+    @description = attrs[:attributes][:description]
+    @category = attrs[:attributes][:category]
+    @cost = attrs[:attributes][:cost]
+    @cost_max = attrs[:attributes][:cost_max]
+    @is_free = attrs[:attributes][:is_free]
+    @url = attrs[:attributes][:event_site_url]
   end
 end
